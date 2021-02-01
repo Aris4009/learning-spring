5055bb3a8135d2103d5255e1378e6b90b00f4224
Aris4009 <slut@vip.qq.com>
2021-02-01 11:08:04 +0800

    完成1.9

--- demo/src/main/java/web/mvc/_1_9_2/AppConfig.java
+++ demo/src/main/java/web/mvc/_1_9_2/AppConfig.java
@@ -3,11 +3,18 @@ package web.mvc._1_9_2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @EnableAsync
 public class AppConfig {
+
+	@Bean
+	public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppConfig.class);
--- demo/src/main/java/web/mvc/_1_9_2/controller/Controller.java
+++ demo/src/main/java/web/mvc/_1_9_2/controller/Controller.java
@@ -2,6 +2,8 @@ package web.mvc._1_9_2.controller;
 
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * 使用etag，http状态码变为304
+	 * 
+	 * @param version
+	 * @return
+	 */
 	@GetMapping("/test")
 	public ResponseEntity<String> test(String version) {
+		log.info("进入了方法");
 		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(30, TimeUnit.DAYS)).eTag(version).body(version);
 	}
 }
